c77376f65e9ef4120e6a42a17ec0b43d804d01c0
Sergey Kanzhelev <S.Kanzhelev@live.com>
2014-12-31 09:24:55 -0800

    example of reporting PageViews

--- src/main/java/com/microsoft/applicationinsights/tests/manual/Program.java
+++ src/main/java/com/microsoft/applicationinsights/tests/manual/Program.java
@@ -2,18 +2,46 @@ package com.microsoft.applicationinsights.tests.manual;
 
 import com.microsoft.applicationinsights.channel.TelemetryClient;
 import com.microsoft.applicationinsights.datacontracts.*;
+import com.microsoft.applicationinsights.extensibility.TelemetryConfiguration;
 import com.microsoft.applicationinsights.util.DefaultTelemetryClient;
 
 import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @SuppressWarnings("ALL")
 class Program
 {
     public static void main(String[] args) throws IOException
     {
+        //This test demonstrate how to track PageViews. It can be used to instrument Eclipse plugin
+        TelemetryConfiguration.getActive().setInstrumentationKey("02ee4ff3-dc13-48e6-9cbb-b1f0dbca1251");
+        DefaultTelemetryClient client = new DefaultTelemetryClient();
+
+        client.getContext().getUser().setAcquisitionDate(new Date());
+        client.getContext().getUser().setId(UUID.randomUUID().toString());
+        client.getContext().getSession().setId(UUID.randomUUID().toString());
+        boolean isFirstSessionForThisUser = true;
+        client.getContext().getSession().setIsFirst(isFirstSessionForThisUser);
+        boolean isNewSessionForUser = true;
+        client.getContext().getSession().setIsNewSession(isNewSessionForUser);
+
+        client.trackPageView("Check in window");
+
+        client.trackPageView("Check out window");
+
+        client.trackEvent("custom event");
+
+        try {
+            Thread.sleep(10000);
+        }
+        catch(Exception e){
+
+        }
+
+        //Test other data items
         TelemetryClient appInsights = new DefaultTelemetryClient();
         appInsights.getContext().getProperties().put("programmatic", "works");
 
