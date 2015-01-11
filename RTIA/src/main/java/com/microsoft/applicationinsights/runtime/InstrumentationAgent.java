5c4ea53b816de93c9dc90b2483698bb35a80d702
Sergey Kanzhelev <sergkanz@microsoft.com>
2015-01-11 09:55:01 +0200

    Prototype of RTIA

---
+++ RTIA/src/main/java/com/microsoft/applicationinsights/runtime/InstrumentationAgent.java
@@ -0,0 +1,14 @@
+package com.microsoft.applicationinsights.runtime;
+
+import java.lang.instrument.Instrumentation;
+
+
+/**
+ * Methods that would have been great to have on maps.
+ */
+public class InstrumentationAgent {
+    public static void premain(String agentArgs, Instrumentation inst) {
+        System.out.println("Executing premain.........");
+        inst.addTransformer(new DurationTransformer());
+    }
+}
