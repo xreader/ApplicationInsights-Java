38c205901746946075933b3de03e6f59bb26d88a
Sergey Kanzhelev <sergkanz@microsoft.com>
2015-01-12 15:19:19 +0200

    Clean up test and make it work with custom servlet

---
+++ Web/src/test/java/com/microsoft/applicationinsights/web/SimpleRequestMonitoringTest.java
@@ -0,0 +1,52 @@
+package com.microsoft.applicationinsights.web;
+
+import com.microsoft.applicationinsights.TelemetryConfiguration;
+import com.microsoft.applicationinsights.telemetry.HttpRequestTelemetry;
+import com.microsoft.applicationinsights.telemetry.Telemetry;
+import com.microsoft.applicationinsights.web.utils.Callback200Servlet;
+import com.microsoft.applicationinsights.web.utils.MockTelemetryChannel;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.server.*;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import static com.microsoft.applicationinsights.web.utils.HttpHelper.sendGetRequestAndWait;
+import javax.servlet.DispatcherType;
+import java.util.EnumSet;
+import java.util.List;
+
+/**
+ * Created by sergkanz on 1/12/2015.
+ */
+public class SimpleRequestMonitoringTest {
+    @Test
+    public void testDefaultPageReturns200() throws Exception {
+        //Set channel
+        MockTelemetryChannel channel = new MockTelemetryChannel();
+        TelemetryConfiguration.getActive().setChannel(channel);
+
+        Server server = new Server(1234);
+        try {
+            //Initialize the server
+            ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+            context.setContextPath("/");
+            context.addServlet(Callback200Servlet.class, "/");
+            context.addFilter(httpfilter.class, "/*", EnumSet.of(DispatcherType.INCLUDE, DispatcherType.REQUEST));
+            server.setHandler(context);
+            server.start();
+
+            sendGetRequestAndWait("http://localhost:1234");
+
+            List<Telemetry> items = channel.getTelemetryItems();
+            assertEquals(1, items.size());
+            HttpRequestTelemetry requestTelemetry = (HttpRequestTelemetry)items.get(0);
+
+            assertEquals("200", requestTelemetry.getResponseCode());
+        }
+        finally {
+            server.stop();
+            server.destroy();
+        }
+    }
+
+}
---
+++ Web/src/test/java/com/microsoft/applicationinsights/web/utils/Callback200Servlet.java
@@ -0,0 +1,23 @@
+package com.microsoft.applicationinsights.web.utils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by sergkanz on 1/12/2015.
+ */
+public class Callback200Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setStatus(200);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setStatus(200);
+    }
+}
+
